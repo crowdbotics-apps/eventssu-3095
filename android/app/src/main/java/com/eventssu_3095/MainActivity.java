ecd152027cb95cf4c0700528bf885cc031f5f7b2
Crowdbotics <team@crowdbotics.com>
2019-05-08 17:31:33 +0000

    initial commit

---
+++ android/app/src/main/java/com/eventssu_3095/MainActivity.java
@@ -0,0 +1,15 @@
+package com.eventssu_3095;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "eventssu_3095";
+    }
+}
